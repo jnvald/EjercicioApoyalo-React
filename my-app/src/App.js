d40fe0014cb6c119d127c12944beeb12ad11ece0
jnvald <jnvald@gmail.com>
2021-05-14 16:57:59 -0400

    Primer proyecto en React

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+     <h1>Hello Dojo!</h1>
+        <p>Things I Need to do:</p>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+        <div className="boton">
+          <button type="submit" >AVANZAR</button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
